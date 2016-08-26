361ecd8735bd337a3a53e2aa6e42be32d0767bac
troussier <troussierd@gmail.com>
2016-08-26 11:01:21 +0900

    first commit

---
+++ imageHover.js
@@ -0,0 +1,118 @@
+/*
+ * 画像をホバー時に変化させるプラグイン
+ * $('.imechen').imechen({
+ *     type: 'transparent',   //【動作タイプ】「半透明」'transparent', 「画像切替」'toggle'
+ *     duration: 200,         //【変化速度】数値を入れる（ms）
+ *     opacity: '.6',         //【動作タイプが半透明の場合の透明度】'1'〜'0'の間の数値
+ *     suffix: '_on'          //【動作タイプが画像切替の場合のon画像ファイル名の接尾語】
+ * });
+ * 
+ * <img src="xxx.jpg" class="imechen">
+ * 
+ * or
+ * 
+ * <a href="#" data-ic-group><img src="xxx.jpg" class="imechen" data-ic-mode="indirect">クリック</a>
+ * 
+ * ■直接モードと間接モード
+ * 直接モードとは指定した画像にホバーした際にその画像をアクションさせるモードです。デフォルト設定です。
+ * 間接モードとは指定した画像を内包する要素にホバーした際にその要素をトリガーとして画像をアクションさせるモードです。
+ * 間接モードにするには起動させるためのセレクタ要素に「data-ic-mode="indirect"」を書きます。
+ * その上でトリガーとしたい要素に「data-ic-group」を書きます。
+ * <a href="#" data-ic-group>
+ *     <img src="xxx.jpg" class="imechen" data-ic-mode="indirect">
+ *     <img src="xxx.jpg" class="imechen" data-ic-mode="indirect">
+ *     間接モード
+ * </a><br>
+ */
+;(function($){
+    'use strict';
+    
+    $.fn.imechen = function(options) {
+        var settings = $.extend({
+            type: 'transparent',
+            duration: 200,
+            opacity: '.6',
+            suffix: '_on',
+            mode: 'direct',
+            groupParent: '[data-ic-group]'
+        }, options);
+        
+        return this.each(function() {
+            var $ic = $(this);
+            var mode = $ic.data('ic-mode') || settings.mode;
+            var type = $ic.data('ic-type') || settings.type;
+            var $trigger;
+            var $wrap = $ic.wrap($('<span class="ic-wrap"></span>')).closest($('.ic-wrap'));
+            
+            switch (type) {
+                
+                case 'transparent':
+                    var originalOpacity;
+                    $trigger = (mode === 'indirect') ? $ic.closest(settings.groupParent) : $ic;
+                    $trigger.on({
+                        'mouseenter': function() {
+                            var $target = (mode === 'indirect') ? $(this).find($ic) : $(this);
+                            $target.stop(true, true);
+                            originalOpacity = $target.css('opacity');
+                            var duration = typeof $ic.data('ic-duration') === 'number' ? $ic.data('ic-duration') : settings.duration;
+                            var opacity = typeof $ic.data('ic-opacity') === 'number' ? $ic.data('ic-opacity') : settings.opacity;
+                            $target.fadeTo(duration, opacity);
+                        },
+                        'mouseleave': function() {
+                            var $target = (mode === 'indirect') ? $(this).find($ic) : $(this);
+                            var duration = typeof $ic.data('ic-duration') === 'number' ? $ic.data('ic-duration') : settings.duration;
+                            $target.stop(true, false).fadeTo(duration, originalOpacity);
+                        }
+                    });
+                    break;
+                    
+                case 'toggle':
+                    var src = $ic.attr('src');
+                    var src_on = src.substr(0, src.lastIndexOf('.')) + settings.suffix + src.substring(src.lastIndexOf('.'));
+                    
+                    $ic.each(function() {
+                        $(this).addClass('ic-off')
+                            .clone()
+                            .attr('src', src_on)
+                            .insertAfter($(this))
+                            .removeClass('ic-off')
+                            .addClass('ic-on');
+                    });
+                    
+                    var wrapStyle = {
+                        'display': 'inline-block',
+                        'position': 'relative'
+                    };
+                    var onStyle = {
+                        'position': 'absolute',
+                        'top': 0,
+                        'left': 0,
+                        'opacity': 0
+                    };
+                    
+                    $ic.closest($wrap).css(wrapStyle)
+                        .children('.ic-on').css(onStyle);
+                        
+                    $trigger = (mode === 'indirect') ? $ic.closest(settings.groupParent) : $ic.closest($wrap);
+                    $trigger.on({
+                        'mouseenter': function() {
+                            var duration = typeof $ic.data('ic-duration') === 'number' ? $ic.data('ic-duration') : settings.duration;
+                            $ic.siblings('.ic-on').stop(true, true).fadeTo(duration, 1, function() {
+                                $ic.stop(true, true).fadeTo(0, 0);
+                            });
+                        },
+                        'mouseleave': function() {
+                            var duration = typeof $ic.data('ic-duration') === 'number' ? $ic.data('ic-duration') : settings.duration;
+                            $ic.stop(true, false).fadeTo(0, 1);
+                            $ic.siblings('.ic-on').stop(true, false).fadeTo(duration, 0);
+                        }
+                    });
+                    break;
+                    
+                default:
+                    console.log('"' + type + '" is no longer available.');
+                    break;
+            }
+        });
+    };
+}(jQuery));
